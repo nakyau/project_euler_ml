769b21b55955a6927bc8b6b9f23d16da53169c10
Nakau Rie <nakyau@gmail.com>
2012-10-02 23:53:33 +0900

    problem 3.

--- p3.ml
+++ p3.ml
@@ -4,20 +4,37 @@ The prime factors of 13195 are 5, 7, 13 and 29.
 What is the largest prime factor of the number 600851475143 ?
 *)
 
-let rec range = function 0 -> [] | n -> range(n-1) @ [n];;
- 
-let factors n =
-  List.filter (fun v -> (n mod v) = 0) (range n);;
+(*
+ * get prime factors of number n
+ *)
+let prime_factors n =
+  let rec iterate i n =
+    if ( i * i > n )
+      then [n]
+      else
+        if ( ( n mod i ) = 0 )
+          then i :: ( iterate i ( n / i ) )
+          else iterate ( i + 1 ) n
+  in
+  iterate 2 n
+;;
 
+(*
+ * get a last value of list
+ *)
 exception Empty_list;;
 let rec last = function
-  [] -> raise Empty_list
-| [_] as xs -> xs
-| x::xs -> last xs;;
+    [] -> raise Empty_list
+  | [_] as xs -> xs
+  | x::xs -> last xs;;
 
+(*
+ * for debug. 
+ *)
 let rec print_list = function 
 [] -> ()
 | e::l -> print_int e ; print_string " " ; print_list l;;
 
-let f = factors 600851475143;;
-print_list(last f);;
+print_list(last (prime_factors 600851475143));;
+
+
